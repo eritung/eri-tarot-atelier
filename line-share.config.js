49a74538b1c78ec4325bf8219777401d88408d2b
Eri <eritung0421@gmail.com>
2026-07-30 22:20:26 +0800

    line-share.config.js を更新

--- line-share.config.js
+++ line-share.config.js
@@ -8,7 +8,6 @@
 window.ERI_LINE_CONFIG = {
   ...(window.ERI_LINE_CONFIG || {}),
   LIFF_ID:
-    window.ERI_LINE_CONFIG?.LIFF_ID ||
-    "2010900471-Lp7tUAMy",
+    window.ERI_LINE_CONFIG?.LIFF_ID || "2010900471-Lp7tUAMy",
   HOME_URL: window.ERI_LINE_CONFIG?.HOME_URL || "",
 };
